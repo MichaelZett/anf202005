2294d41b248f5063a0c86065faf302c712b1d614
MichaelZett <michael2.zoeller@gmail.com>
2020-05-30 17:01:04 +0200

    Last commit part 1

--- src/feu/BigCalculatorApp.java
+++ src/feu/BigCalculatorApp.java
@@ -6,19 +6,22 @@ public class BigCalculatorApp {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-
 		int zahl = 0;
 
 		do {
 			System.out.println("Gib eine Zahl ein!");
-			while (!scanner.hasNextInt()) {
-				System.out.println("Das ist keine Zahl!");
-				scanner.next();
-			}
+			getNumberFromUser(scanner);
 			zahl = zahl + scanner.nextInt();
 		} while (!scanner.hasNext("calc"));
 		System.out.println("Die Summe ist: " + zahl);
 		scanner.close();
+	}
+
+	private static void getNumberFromUser(Scanner scanner) {
+		while (!scanner.hasNextInt()) {
+			System.out.println("Das ist keine Zahl!");
+			scanner.next();
+		}
 	}
 
 }
--- src/feu/Schaltjahr.java
+++ src/feu/Schaltjahr.java
@@ -6,10 +6,12 @@ public class Schaltjahr {
 		boolean result2020 = Schaltjahr.isSchaltjahr(2020); // wahr
 		boolean result2000 = Schaltjahr.isSchaltjahr(2000); // wahr
 		boolean result1900 = Schaltjahr.isSchaltjahr(1900); // falsch
+		boolean result2003 = Schaltjahr.isSchaltjahr(2003); // falsch
 
 		System.out.println("1900: " + result1900 //
 				+ " - 2000: " + result2000 //
-				+ " - 2020: " + result2020);
+				+ " - 2020: " + result2020 //
+				+ " - 2003: " + result2003);
 	}
 
 	private static boolean isSchaltjahr(int jahr) {
@@ -20,7 +22,14 @@ public class Schaltjahr {
 //	c) Ein Jahr ist ebenfalls ein Schaltjahr, 
 		// wenn die Jahreszahl durch 4 und durch 400 teilbar ist.
 
-		return false;
+		boolean nichtTeilbarDurch4 = jahr % 4 != 0;
+		if (nichtTeilbarDurch4) {
+			return false;
+		} else {
+			boolean teilbarDurch400 = jahr % 400 == 0;
+			boolean nichtTeilbarDurch100 = jahr % 100 != 0;
+			return teilbarDurch400 || nichtTeilbarDurch100;
+		}
 	}
 
 }
